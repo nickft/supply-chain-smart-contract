54bcd17300156f4ca10e9f31bfb1da427d5f3449
Nikos Fotos <nickfotosgr@gmail.com>
2022-01-07 22:30:58 +0100

    Structure comments and description

--- supply-chain.sol
+++ supply-chain.sol
@@ -6,212 +6,172 @@ pragma solidity ^0.7.0;
     Create a trusted third party that stores the money until 
     the delivery of the product takes place. Like Amazon
 
-    1. Buyer deposits 1 ETH as payment
-    2. Seller - that has created the SC - sees the money in the SC
-    3. Basically Seller has defined two functions:
-        -> depositPayment()
-        -> confirmPayment()
-    4. These functions must be executed only by the buyer
-    5. Once Seller sees the money, he sends the product
-    6. Once the Buyer receives the product - and he's happy - 
-    he sends confirm delivery in order for the ETH to be transfered 
-    from the SC to the Seller's account
+    The functionality of the Smart Contract is presented in the following brief case scenario:
 
-*/
-
-/*
-
-    Approach:
-
-    1. First we need to define the storage of the SC
-    2. Seller will be defined once the SC is deployed
-    3. Buyer will be defined once depositPayment() is called
-    4. For depositPayment we need to define requires and actions
-    5. requires: 
-        a) Buyer should have money 
-        b) There is no buyer defined so far (address(0))
-
-        actions:
-        define the buyer as the sender of the transaction
-    6. For confirmPayment we need to transfer the money to the seller
-        -> first we need to make the seller's address as payable
-        -> and in the constructor cast the initialization as payable
-
-        requires:
-          a) The sender of the transaction is the buyer
-
-        actions:
-        transfer 1 ether to the seller's account
-
-    ----------------------------------------------------------------
-    IDEAS TO extend smart contracts (see Escrow smart contracts)
-
-    What could go wrong? 
-
-    1) What if seller does not send the product?
-    2) What if buyer never calls confirm?
-
-    So far, we don't give any motive/obligation for the buyer/seller 
-    to proceed with the process.
-
-    Motive for the Seller: Seller sends 1 ETH when SC is deployed
-    Motive for the Buyer: Make the Buyer put 2 ETH and upon confirmation receive the 1 ETH back
-
-    Force the Buyer to send confirmation: Give a deadline to respond
-
-
-
-*/
-
-/**
-    For the second part of the first ampliation what we want to do is too add information of the product so It can be checked when creating the smart contract. And modified.
-    The information we want to add is:
-        Name of the product
-        price of the product
-    Functionalities:
-        Check name and price.
-            Make variables public so easy to check
-        Modify the price of the product if it has not been bought.
-            We will need a variable to indicate if the product has been bought.
+    1) A Seller (S) publishes (through the Smart Contract) an item for sale 
+       providing name and price namely, "Ticket to Barcelona Match" and 1 Eth.
+    2) A Buyer (B) wants to purchase so she deposits product's price + security deposit as a payment.
+       In our case the value = equals to 2 Eth.
+    3) S sees the money in the Smart Contract and sends the item to B.
+    4) Assuming that the shipment is not lost along the way, eventually B will receive her package.
+    5) B needs to confirm the delivery of the product. If B confirms the delivery on time (e.g. under 30 days after the arrival) then
+       S is getting paid with the price of the product while B receives hers security deposit intact. However, if B does not 
+       confirm it on time, she receives a penalty which means that S will get both the product value as well as the security deposit.
+    6) If B wants to return the product she has a specific period to do so (i.e. 10 days).
+    7) Once S gets notified about B's decision, she sends the money (refund) to the smart contract. And waits for the item to return to S.
+    8) Once S has received the product, she confirms the delivery and the refund is successfull
+    9) As in step 4) we assume that the shipment is not lost and eventually S will receive the package.
+    9) S is being given a specific period of time (i.e. 10 days) to confirm the return of the product. If she confirms on time the money will
+    be returned to B. If not, B has also the ability to withdraw the money from the smart contract once the aforementioned period has passed.
 */
 
 contract Amazon {
 
-    uint public constant SECURITY_DEPOSIT_IN_ETHER = 1;
+    uint public constant SECURITY_DEPOSIT_IN_ETHER = 1; //
 
-    /*** Storage ***/
+    /* Entities */
     address payable seller;
     address payable buyer;
+
+    /* Important Dates */
     uint256 public purchaseDate;
     uint256 public confirmationDate;
     uint256 public returnDate;
-    bool returnRequested;
-    /** Data about the product **/
-    string public productName;
-    uint32 public price; //IN ETHER 
-    bool productBought; //Does not need initialization to false because done by default by Solidity
+    
+    /* Flags */
+    bool returnRequested; // Whether a refund has been requested
 
-    /**
-        For variables of type array we need to specify an explicit data location.
-        Data location can be storage or memory.
-        storage is for persistent data and memory is for volatile data (as it is non persistent)
-        In our case we will copy the value from memory to the storage we already have. 
-    */
+    /* Data about the product */
+    string public productName;
+    uint32 public price; // In Ether 
+    
+
+    /* The seller deployes the smart contract by providing the price and the name of the product */
     constructor(uint32 _sellPrice, string memory _name) {
         seller = payable(msg.sender);
         productName = _name;
         price = _sellPrice;
     }
 
+    /* Modifier to define if the one that makes the transaction is the seller of the product */
     modifier isSeller() {
         require(msg.sender == seller, "Only the seller can perform this function");
         _;
     }
 
+
+    /* Modifier to define if the one that makes the transaction is the buyer of the product */
     modifier isBuyer(){
         require(msg.sender == buyer, "Only the client can perform this function");
         _;
     }
 
-    /*** Functions ***/
+    /* 
+        Method that allows only the seller to modify the price of the product provided that
+        the product hasn't been purchased yet.
+    */
+    function changePrice(uint32 _newSellPrice) external payable isSeller {
+        // A product is already purchased if there is a buyer != 0
+        require(buyer == address(0), "A price cannot be changed if the product is bought.");
+
+        price=_newSellPrice;    
+    }
+
+    /* 
+        Method that allows a user to purchase the product.
+    */
     function depositPayment() external payable {
-        // 5a
         require(buyer == address(0), "The product has already been purchased.");
 
-        // 5b
-        require(!productBought, "The amount of money for the product has already been deposited.");
-
-        // 5c
-        // We cannot use "ether" keyword with variables, only constants. but is equivalent to *10^18
+        // The value of the transaction shall be equal to the price of the product + the security deposit
         require(msg.value == (price + SECURITY_DEPOSIT_IN_ETHER) * 10**18, "There is not enough money in the transaction to cover the price plus the security deposit.");
 
-
-        // action
+        // Store the buyer's address as well as the date of the purchase
         buyer = msg.sender;
-        productBought = true;
         purchaseDate = block.timestamp;
     }
 
-    function confirmDelivery() external payable isBuyer {
-        // 30 days to epochs = 2592000 
+    /* 
+        Method that allows the buyer to confirm the delivery of the product. 
 
-        //DEBUG: 1 minute to epochs = 60
-        if(block.timestamp - purchaseDate < 60){
+        Case a) If the delivery confirmation is on time (in 10 days) seller will get the price of the product and the security deposit
+        is returned to the buyer
+
+        Case b) If the delivery confirmation is overdue, seller will receive both the product price and the deposit. 
+    */
+    function confirmDelivery() external payable isBuyer {
+        
+        // 10 days to epochs = 864000    
+        if(block.timestamp - purchaseDate < 86400){
             seller.transfer(price * 10**18);
             buyer.transfer(SECURITY_DEPOSIT_IN_ETHER * 10**18);
         } else {
             seller.transfer( (price + SECURITY_DEPOSIT_IN_ETHER) * 10**18);
         }
+
+        // Store the date of the delivery confirmation
         confirmationDate = block.timestamp;
     }
 
-    /**
-        Function for the seller to call if the expirationTime has passed an the client does not confirm the delivery.
-        We are assuming that all the items arrive to the destiny, if not Seller could get the money an maybe client never receive the product.
+    /*
+        Method that allows the seller to withdraw the money from the smart contract provided that the buyer's deadline 
+        to confirm the delivery has passed.
     */
     function receiveDeposit() external payable isSeller {
+        // Check if a return order has been requested.
         require(returnRequested == false, "Cannot retreive the money if the return is issued");
-        if (block.timestamp - purchaseDate > 60) {
+
+        // 10 days to epochs = 864000
+        if (block.timestamp - purchaseDate > 864000) {
             seller.transfer((price+SECURITY_DEPOSIT_IN_ETHER) * 10**18);
         }
     }
 
-    /** Note: 
-        Difference between external and public is that public methods can be called by 
-        inside the contract, other contracts that inherit the contract and other contracts and accounts.
-        external contracts can only be called by other contracts or accounts.
+    /*
+        This method allows the Seller to issue a "Return product" request. If this request is issued on time (10 days), 
+        the Seller returns the money he received back to the smart contract.  
     */
-    function changePrice(uint32 _newSellPrice) external payable isSeller {
-        require(productBought==false, "A price cannot be changed if the product is bought.");
+    function returnIssued() external payable isSeller {
 
-        price=_newSellPrice;    
-    }
-
-     /**
-        Return Policy:
-            The buyer can return the item.
-            The seller will confirm when te item is received and pay the price to the buyer.
-
-        Problem: The money must be in the smart contract first! (The money has already been transfered!!)
-        So we need like a protocol.
-        
-        Buyer: Requests to return. (I did not implement this, like empirical function I thought)
-        Seller: Accepts request and deposits price into the smart contract.
-        Seller: Receives the item and confirms in order to pay the buyer back.
-
-        I don't think that the seller needs an incentive to confirm the return.
-        I will develop a function for the buyer to be able to recover the seller's money if some time has passed.
-
-        NOTE: I'm Assuming the items are never lost and always arrive within a period of time.
-    */
-
-    /**
-        The Buyer is able to send the item back. 
-        If the buyer sends the item back, the seller will  be notified and will store to the Smart Contract the price that the Buyer payed to buy the item.
-        
-    */
-    function itemReturn() external payable isSeller {
+        // Make sure that the transaction contains the price of the product. 
         require(msg.value == price * 10**18, "The price of the item was different");
-        require(block.timestamp - confirmationDate < 120, "The Period to return the product has expired");
+
+        // 7 days to epochs = 864000  
+        require(block.timestamp - confirmationDate < 864000, "The Period to return the product has expired");
         
+        // Set the returnRequested flag to "True" and store the date of the return request.
         returnDate=block.timestamp;
         returnRequested=true;
     }
 
+    /*
+        This method allows the Seller to confirm that the product has been delivered. This can be only executed
+        if a return has been requested in the first place
+    */
     function confirmReturnReceived() external payable isSeller {
+        // Make sure that a return has been requested
         require(returnRequested==true, "The return request has not been issued.");
+
+        // Refund the Buyer
         buyer.transfer(price* 10**18);
         returnRequested=false;
     }
 
+    /*
+        This method allows the Buyer to withdraw the money that the Seller has put in the smart contract provided
+        that the Seller hasn't confirmed the return of the product after a period of time has passed after the return
+        has been issued. This period is set to 10 days
+    */
     function buyerReclaimReturnPrice() external payable isBuyer {
+        // Make sure that a return has been requested
         require(returnRequested==true, "The return request has not been issued.");
-        require(block.timestamp - returnDate > 60, "The time needed to retreive the money is not met.");
 
+        // 10 days to epochs = 864000   
+        require(block.timestamp - returnDate > 864000, "Refund is under processing");
+
+        // Refund the Buyer
         buyer.transfer(price * 10**18);
         returnRequested=false;
     }
-
-
     
 }
